94c1b8fc2762a834dfe2742d4377de69e6f33b06
wrya <wrya141@gmail.com>
2026-03-16 00:49:10 +0300

    making it production ready

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -1,4 +1,6 @@
 import { treaty } from "@elysiajs/eden";
 import type { App } from "../app/api/[[...slugs]]/route";
 
-export const client = treaty<App>("localhost:3000").api;
+export const client = treaty<App>(
+  "https://real-time-temp-chat-git-main-wrya141s-projects.vercel.app/",
+).api;
